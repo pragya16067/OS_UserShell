e7df25960287ab3f2492003fe1d898f60d992e78
pragya16067 <pragya16067@iiitd.ac.in>
2018-01-19 01:08:01 +0530

    rm completed

--- bin/rm.c
+++ bin/rm.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+
+//Options : -i and -v
 
 int main(int argc, char *argv[]) {
+	char *path;
+	FILE *f;
 	if(argc < 2){
 		printf("Insufficient arguments \n");
 		return 1;
 	} 
 	
 	if(argc == 2) {
-		char *path = argv[1];
-		FILE *f;
+		path = argv[1];
 		f= fopen(path, "r");
 		if(f != NULL) //the file exists
 			remove(path);
@@ -19,9 +23,53 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	
-	else
+	else if(argc == 3)
 	{
-		
+		if(strcmp(argv[1],"-i")==0) //option is -i
+		{
+			path = argv[2];
+			printf("Do you want to remove file '%s'? (y/n):",path);
+			char c=getchar();
+			if(c=='y')
+			{
+				//remove the file
+				
+				f= fopen(path, "r");
+				if(f != NULL) //the file exists
+					remove(path);
+				else
+				{
+					printf("Cannot remove '%s': No such file\n",path);
+					return 1;
+				}
+			}
+				
+		}
+		else if(strcmp(argv[1],"-v")==0) //option is -v (verbose)
+		{
+			path = argv[2];
+			//remove the file
+			
+			f= fopen(path, "r");
+			if(f != NULL) //the file exists 
+			{
+				remove(path);
+				printf("removed '%s'\n",path);
+			}
+			else
+			{
+				printf("Cannot remove '%s': No such file\n",path);
+				return 1;
+			}
+			
+				
+		}
+		else
+		{
+			printf("Please enter a valid option.\n");
+		}
+
+
 	}
 	return 0;
 }
