739c7d87192319666dbc656cb009af0606c2d50e
pragya16067 <pragya16067@iiitd.ac.in>
2018-01-19 13:05:56 +0530

    one option done

--- bin/mkdir.c
+++ bin/mkdir.c
@@ -1,20 +1,49 @@
-
+#include <string.h>
 #include <sys/stat.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+//options implemented are -v and 
 
-	char *path = argv[1];
-	
-	if ((mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH )) == 0) 
+
+int main(int argc, char *argv[]) {
+	if(argc==2) //no options for mkdir 
 	{
-		
-  		printf("Directory created\n");
-  	}
-	else {
-  		// could not open directory 
-		printf("could not open directory\n");
-  		return 1;
+		char *path = argv[1];
+	
+		if ((mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH )) == 0) 
+		{ }
+		else {
+	  		// could not create directory 
+			printf("Could not create directory\n");
+	  		return 1;
+		}
+	}
+	else if(argc==3) {
+		if(strcmp(argv[1],"-v")==0) //option is -v
+		{
+			char *path = argv[2];
+	
+			if ((mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH )) == 0) 
+			{
+		  		printf("mkdir: created directory '%s'\n",path);
+		  	}
+			else {
+		  		// could not create directory 
+				printf("Could not create directory\n");
+		  		return 1;
+			}
+		}
+		else
+		{
+			printf("Please enter a valid option");
+			return 1;
+		}
+	
+	}
+	else
+	{
+		printf("Please enter a valid option");
+		return 1;
 	}
 	return 0;
 }
